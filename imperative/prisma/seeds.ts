e2311bbeddb54c09ad27152cb951a7d470558e65
maaaashi <doyaseizin.7@gmail.com>
2025-05-16 15:00:17 +0900

    seed

---
+++ imperative/prisma/seeds.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.article.create({
+    data: {
+      id: 1,
+      title: "【CSS】flex-growを使おう",
+      url: "https://qiita.com/maaaashi/items/13cdf02c394cf9668de7",
+      createdAt: new Date(),
+      tagedArticles: {
+        create: [{ tag: { create: { name: "CSS" } } }],
+      },
+    },
+  });
+  await prisma.article.create({
+    data: {
+      id: 2,
+      title: "再利用性を高めたWeb Components",
+      url: "https://qiita.com/maaaashi/items/3376cdd0b6ee75c634eb",
+      createdAt: new Date(),
+      tagedArticles: {
+        create: [
+          { tag: { create: { name: "JavaScript" } } },
+          { tag: { create: { name: "WebComponents" } } },
+          { tag: { create: { name: "TypeScript" } } },
+          { tag: { create: { name: "Lit" } } },
+        ],
+      },
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
